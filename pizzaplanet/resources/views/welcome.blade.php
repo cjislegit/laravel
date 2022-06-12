e8b6eb69f51c6ba9db0a7c39e71139771b52654a
Carlos Ramirez <carlosjramirez11@gmail.com>
2022-06-11 17:46:17 -0700

    Set up form to create new orders

--- pizzaplanet/resources/views/welcome.blade.php
+++ pizzaplanet/resources/views/welcome.blade.php
@@ -1,26 +1,27 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-    <div class="top-right links">
-        @auth
-        <a href="{{ url('/home') }}">Home</a>
-        @else
-        <a href="{{ route('login') }}">Login</a>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+            <div class="top-right links">
+                @auth
+                    <a href="{{ url('/home') }}">Home</a>
+                @else
+                    <a href="{{ route('login') }}">Login</a>
 
-        @if (Route::has('register'))
-        <a href="{{ route('register') }}">Register</a>
+                    @if (Route::has('register'))
+                        <a href="{{ route('register') }}">Register</a>
+                    @endif
+                @endauth
+            </div>
         @endif
-        @endauth
-    </div>
-    @endif
 
-    <div class="content">
-        <img src="/img/pizza-house.png" alt="Pizza house logo">
-        <div class="title m-b-md">
-            The North's Best Pizzas
+        <div class="content">
+            <img src="/img/pizza-house.png" alt="Pizza house logo">
+            <div class="title m-b-md">
+                The North's Best Pizzas
+            </div>
+            <a href="/pizzas/create">Order a Pizza</a>
         </div>
     </div>
-</div>
 @endsection
