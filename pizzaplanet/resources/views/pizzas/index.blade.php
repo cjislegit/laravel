7439b2715368d90324fdf0861dd438fd8634d991
Carlos Ramirez <carlosjramirez11@gmail.com>
2022-06-12 14:11:01 -0700

    Added styling to all pages

--- pizzaplanet/resources/views/pizzas/index.blade.php
+++ pizzaplanet/resources/views/pizzas/index.blade.php
@@ -25,32 +25,17 @@ echo($name);
 {{-- Blade loop --}}
 <?php /*@for($i = 0; $i < 5; $i++) <p>the value of i is {{$i}}</p>
 @endfor */?>
-
-<div class="flex-center position-ref full-height">
-    @if (Route::has('login'))
-    <div class="top-right links">
-        @auth
-        <a href="{{ url('/home') }}">Home</a>
-        @else
-        <a href="{{ route('login') }}">Login</a>
-
-        @if (Route::has('register'))
-        <a href="{{ route('register') }}">Register</a>
-        @endif
-        @endauth
-    </div>
-    @endif
-
-    <div class="content">
-        <div class="title m-b-md">
-            Pizza List
+<div class="wrapper pizza-index">
+    <h1>Pizza Orders</h1>
+    @foreach($pizzas as $pizza)
+        <div class="pizza-item">
+            <img src="/img/pizza.png" alt="pizza icon">
+            <h4><a href="/pizzas/{{$pizza->id}}">{{ $pizza->name}}</a></h4>
         </div>
-        <div>{{$name}}</div>
-        @foreach($pizzas as $pizza)
-        <div>
-    {{ $pizza->name}} - {{$pizza->type}} - {{$pizza->base}}
-        </div>
-        @endforeach
-    </div>
+    @endforeach
 </div>
+
+
+        
+
 @endsection
